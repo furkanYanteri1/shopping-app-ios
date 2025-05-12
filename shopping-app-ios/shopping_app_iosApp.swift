f4080d88a7fa6c7ea4de4e93da8822877a753b17
Furkan Yanteri <furkanyanteri@gmail.com>
2025-05-12 20:01:14 +0300

    Initial Commit

---
+++ shopping-app-ios/shopping_app_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  shopping_app_iosApp.swift
+//  shopping-app-ios
+//
+//  Created by Furkan Yanteri on 12.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct shopping_app_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
